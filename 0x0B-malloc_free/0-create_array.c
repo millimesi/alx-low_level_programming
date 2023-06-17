ee4bb22c27d5996a2a9097b387484954c62cc91d
millimesi <millionmesi1@gmail.com>
2023-06-17 12:15:25 +0300

    compilation solved

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,7 +21,7 @@ char *create_array(unsigned int size, char c)
 		arr = malloc(size * sizeof(char));
 		if (arr != NULL)
 		{
-			int i;
+			unsigned int i;
 
 			for (i = 0; i < size; i++)
 			{
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -1,4 +1,4 @@
-#include "tmain.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
